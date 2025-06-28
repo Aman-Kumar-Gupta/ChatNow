18d44eef95d5efc9d13be0577eaf26919b52a17f
Aman <amankr250804@gmail.com>
2025-06-28 21:08:54 +0530

    file name issue(case inconsistency)

--- frontend/context/ChatContext.jsx
+++ frontend/context/ChatContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { AuthContext } from "./authContext";
 import toast from "react-hot-toast";
+import { AuthContext } from "./authContext";
 
 export const ChatContext=createContext();
 
